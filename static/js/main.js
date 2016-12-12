4836031b8253aa6b0848d5343c108701c2e3f499
K.C.Saff <kc@saff.net>
2016-12-12 14:36:01 -0600

    include opposing army

--- static/js/main.js
+++ static/js/main.js
@@ -56,33 +56,56 @@ $(document).ready(function() {
 		}
 	};
 	var START_POSITION = {
-		P1: ["P", 7, 1],
-		P2: ["P", 7, 2],
-		P3: ["P", 7, 3],
-		P4: ["P", 7, 4],
-		P5: ["P", 7, 5],
-		P6: ["P", 7, 6],
-		P7: ["P", 7, 7],
-		P8: ["P", 7, 8],
-		P9: ["P", 7, 9],
-		B:  ["B", 8, 2],
-		R:  ["R", 8, 8],
-		L1: ["L", 9, 1],
-		N2: ["N", 9, 2],
-		S3: ["S", 9, 3],
-		G4: ["G", 9, 4],
-		K:  ["K", 9, 5],
-		G6: ["G", 9, 6],
-		S7: ["S", 9, 7],
-		N8: ["N", 9, 8],
-		L9: ["L", 9, 9]
+		MP1: ["P", 7, 1, "my"],
+		MP2: ["P", 7, 2, "my"],
+		MP3: ["P", 7, 3, "my"],
+		MP4: ["P", 7, 4, "my"],
+		MP5: ["P", 7, 5, "my"],
+		MP6: ["P", 7, 6, "my"],
+		MP7: ["P", 7, 7, "my"],
+		MP8: ["P", 7, 8, "my"],
+		MP9: ["P", 7, 9, "my"],
+		MB:  ["B", 8, 2, "my"],
+		MR:  ["R", 8, 8, "my"],
+		ML1: ["L", 9, 1, "my"],
+		MN2: ["N", 9, 2, "my"],
+		MS3: ["S", 9, 3, "my"],
+		MG4: ["G", 9, 4, "my"],
+		MK:  ["K", 9, 5, "my"],
+		MG6: ["G", 9, 6, "my"],
+		MS7: ["S", 9, 7, "my"],
+		MN8: ["N", 9, 8, "my"],
+		ML9: ["L", 9, 9, "my"],
+		TP1: ["P", 3, 1, "their"],
+		TP2: ["P", 3, 2, "their"],
+		TP3: ["P", 3, 3, "their"],
+		TP4: ["P", 3, 4, "their"],
+		TP5: ["P", 3, 5, "their"],
+		TP6: ["P", 3, 6, "their"],
+		TP7: ["P", 3, 7, "their"],
+		TP8: ["P", 3, 8, "their"],
+		TP9: ["P", 3, 9, "their"],
+		TB:  ["B", 2, 8, "their"],
+		TR:  ["R", 2, 2, "their"],
+		TL1: ["L", 1, 1, "their"],
+		TN2: ["N", 1, 2, "their"],
+		TS3: ["S", 1, 3, "their"],
+		TG4: ["G", 1, 4, "their"],
+		TK:  ["K", 1, 5, "their"],
+		TG6: ["G", 1, 6, "their"],
+		TS7: ["S", 1, 7, "their"],
+		TN8: ["N", 1, 8, "their"],
+		TL9: ["L", 1, 9, "their"],
 	};
-	function add_piece(piece_name, piece_id, square) {
+	function put_piece(piece_id, piece_type, owner, square) {
 		var width_ratio = square.width() / sheet_map.piece_size[0];
 		var height_ratio = square.height() / sheet_map.piece_size[1];
 		var ratio = Math.min(width_ratio, height_ratio);
+		$("#" + piece_id).remove();
 		square.html(
-			'<div class="piece" id="'
+			'<div class="'
+			+ owner
+			+ ' piece" id="'
 			+ piece_id
 			+ '" style="min-width: ' 
 			+ (sheet_map.piece_size[0] * ratio)
@@ -91,14 +114,13 @@ $(document).ready(function() {
 			+ 'px;"><img src="'
 			+ sheet_map.img_src
 			+ '" style="left: '
-			+ (-sheet_map.pieces[piece_name][0] * ratio)
+			+ (-sheet_map.pieces[piece_type][0] * ratio)
 			+ 'px; top: '
-			+ (-sheet_map.pieces[piece_name][1] * ratio)
+			+ (-sheet_map.pieces[piece_type][1] * ratio)
 			+ 'px; max-width: '
 			+ (sheet_map.img_size[0] * ratio)
 			+ 'px;"></div>'
 		);
-		console.log("asdffsd");
 	};
 	function get_square(row, column) {
 		var index = ((row-1) * 9 + column);
@@ -107,7 +129,7 @@ $(document).ready(function() {
 	function display_position(position) {
 		Object.keys(position).forEach(function(k) {
 			var data = position[k];
-			add_piece(data[0], k, get_square(data[1], data[2]));
+			put_piece(k, data[0], data[3], get_square(data[1], data[2]));
     	});
 	}
 	display_position(START_POSITION);	
